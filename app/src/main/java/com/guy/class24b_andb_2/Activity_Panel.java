be4b4ee38e07e6a4fa42a2a45a5b9339fde79fd9
guy <guyskv@gmail.com>
2024-06-02 18:18:28 +0300

    Initial commit

---
+++ app/src/main/java/com/guy/class24b_andb_2/Activity_Panel.java
@@ -0,0 +1,56 @@
+package com.guy.class24b_andb_2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.button.MaterialButton;
+
+public class Activity_Panel extends AppCompatActivity {
+
+    private AppCompatImageView panel_IMG_question;
+    private MaterialButton[] panel_BTN_answers;
+    private AppCompatImageView[] panel_IMG_hearts;
+
+    private GameManager gameManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel);
+
+        findViews();
+
+        gameManager = new GameManager();
+        next();
+
+    }
+
+    private void next() {
+        String image = gameManager.getCurrentImage();
+        Glide.with(this).load(image).into(panel_IMG_question);
+
+        String[] answers = gameManager.getAnswers();
+        for (int i = 0; i < 4; i++) {
+            panel_BTN_answers[i].setText(answers[i]);
+        }
+    }
+
+    private void findViews() {
+        panel_IMG_question = findViewById(R.id.panel_IMG_question);
+        panel_BTN_answers = new MaterialButton[]{
+                findViewById(R.id.panel_BTN_answer0),
+                findViewById(R.id.panel_BTN_answer1),
+                findViewById(R.id.panel_BTN_answer2),
+                findViewById(R.id.panel_BTN_answer3),
+        };
+        panel_IMG_hearts = new AppCompatImageView[]{
+                findViewById(R.id.panel_IMG_hearts0),
+                findViewById(R.id.panel_IMG_hearts1),
+                findViewById(R.id.panel_IMG_hearts2),
+                findViewById(R.id.panel_IMG_hearts3),
+        };
+    }
+}
